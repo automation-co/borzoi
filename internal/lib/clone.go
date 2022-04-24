e4a6988e35a0347c119fce7ddb9ad6529221910d
Aarush-Goyal <goyalaarush92@gmail.com>
2022-04-24 22:39:19 +0530

    feat(clone): clone implementation

--- internal/lib/clone.go
+++ internal/lib/clone.go
@@ -5,12 +5,25 @@ import (
 	"os"
 
 	"github.com/automation-co/borzoi/internal/config"
+	"github.com/go-git/go-git/v5"
 )
 
-// TASKS TODO:
-// // 1. Handle the case where the config file is empty
-// // 2. Handle the path management
-// 3. Clone the repositories and its error handling
+func Clone() {
+
+	fmt.Println("Cloning the repositories...")
+
+	// Read the config file
+	conf := config.ReadConfig()
+
+	// Get the path of the current directory
+	path := os.Getenv("PWD")
+
+	// Iterate over the repositories
+	iterate(conf, path)
+
+}
+
+// -----------------------------------------------------------------------------
 
 func iterate(conf map[string]interface{}, path string) {
 	// Iterate over the repositories
@@ -21,18 +34,41 @@ func iterate(conf map[string]interface{}, path string) {
 			break
 		}
 
-		newPath := path + "/" + title
-		fmt.Println(newPath)
+		// Check if the directory exists and if not create it
+		if _, err := os.Stat(path); os.IsNotExist(err) {
+			// Create the directory
+			os.Mkdir(path, 0777)
+		}
 
 		if body == nil {
 			break
 		} else {
 
-			// if the body is a map, that has a key "repo" then we have a repository
-			// and we can clone it so for now lets print the value of the key "repo"
+			// if the body is a map, that has a key "repo" then we have a
+			// repository and we can clone it
 			if body.(map[string]interface{})["repo"] != nil {
-				fmt.Println(body.(map[string]interface{})["repo"])
+
+				// Clone the repository at the path
+				fmt.Println(
+					"Cloning " +
+						body.(map[string]interface{})["repo"].(string) +
+						" repo in dir " + path,
+				)
+
+				_, err := git.PlainClone(path+"/"+title, false, &git.CloneOptions{
+					URL:               body.(map[string]interface{})["repo"].(string),
+					RecurseSubmodules: git.DefaultSubmoduleRecursionDepth,
+				})
+
+				if err != nil {
+					fmt.Println(err)
+				} else {
+					fmt.Println("Repository cloned")
+				}
+
 			}
+
+			newPath := path + "/" + title
 
 			iterate(body.(map[string]interface{}), newPath)
 
@@ -41,21 +77,4 @@ func iterate(conf map[string]interface{}, path string) {
 	}
 }
 
-func Clone() {
-
-	fmt.Println("Cloning the repositories...")
-
-	// Read the config file
-	conf := config.ReadConfig()
-
-	// Print the config file contents
-	fmt.Println("Config file contents:")
-	// fmt.Println(conf)
-
-	// Get the path of the current directory
-	path := os.Getenv("PWD")
-
-	// Iterate over the repositories
-	iterate(conf, path)
-
-}
+// -----------------------------------------------------------------------------
